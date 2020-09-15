53399775eaff17e45a4b16be7423ea0fe4bcfc5c
janghyeonan <ajh910@gmail.com>
2020-09-15 23:48:55 +0900

    run commit

---
+++ p_test.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 9)
+
+    @task
+    def my_task(self):
+        self.client.get("/")
+
+#test commadn : locust -f p_test.py --host=http://localhost:8089
